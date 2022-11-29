5dfc9f1f3f4b7dd2ced015c4b06be3cbb6578565
MaiQD <q.dat2710@gmail.com>
2022-11-29 15:39:03 +0700

    auth and basic manage

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ pip install djoser
 pip install pillow
 pip install stripe
 
+django-admin startproject djackets_django
 ## Migration database
 python .\manage.py makemigrations
 python .\manage.py migrate
@@ -19,6 +20,7 @@ python .\manage.py migrate
 python .\manage.py createsuperuser
 python .\manage.py runserver
 
+python .\manage.py startapp product
 
 ## VueJS
 npm install -g @vue/cli
--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import ProductView from '../views/ProductView'
+import CategoryView from '../views/CategoryView'
+import SearchView from '../views/SearchView'
+import CartView from '../views/CartView'
+import SignUpView from '../views/SignUpView'
+import LogInView from '../views/LogInView'
 const routes = [
   {
     path: '/',
@@ -14,7 +19,31 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  }, {
+    path: '/:category_slug/:product_slug/',
+    name: 'Product',
+    component: ProductView
+  }, {
+    path: '/:category_slug',
+    name: 'Category',
+    component: CategoryView
+  }, {
+    path: '/search',
+    name: 'Search',
+    component: SearchView
+  },{
+    path: '/cart',
+    name: 'Cart',
+    component: CartView
+  },{
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUpView
+  },{
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogInView
+  },
 ]
 
 const router = createRouter({
